5eb62e37ef37384756a59317ef09251c543e4aed
Paul Yuan <pyuana@gmail.com>
2015-01-06 11:47:25 -0600

    test annotation for map view

--- src/swift/Koolie/com/modules/map/MapViewController.swift
+++ src/swift/Koolie/com/modules/map/MapViewController.swift
@@ -8,12 +8,29 @@
 
 import Foundation
 import UIKit
+import MapKit
 
 class MapViewController:UIViewController
 {
     
     @IBOutlet weak var backButton:RoundIconButton?
     @IBOutlet weak var listButton:RoundIconButton?
+    @IBOutlet weak var map:MKMapView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let location:CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 51.50007773, longitude: -0.1246402)
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.05, 0.05)
+        let region:MKCoordinateRegion = MKCoordinateRegion(center: location, span: span)
+        self.map?.setRegion(region, animated: true)
+        
+        let annotation:MKPointAnnotation = MKPointAnnotation()
+        annotation.setCoordinate(location)
+        annotation.title = "Big Ben"
+        annotation.subtitle = "London"
+        self.map?.addAnnotation(annotation)
+    }
     
     @IBAction func onBackButton(sender:AnyObject?) {
         self.navigationController?.popViewControllerAnimated(true)
